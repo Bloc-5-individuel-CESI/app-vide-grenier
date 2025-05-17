794efe7c542b60be67456eff7207565b33ca89ba
Pimer0 <zefun74@gmail.com>
2025-05-17 19:37:01 +0200

    ajout d'un formulaire de contacte. En rod, fournir les données du SMTP

--- public/index.php
+++ public/index.php
@@ -49,6 +49,7 @@ $router->add('logout', ['controller' => 'User', 'action' => 'logout', 'private' 
 $router->add('account', ['controller' => 'User', 'action' => 'account', 'private' => true]);
 $router->add('product', ['controller' => 'Product', 'action' => 'index', 'private' => true]);
 $router->add('product/{id:\d+}', ['controller' => 'Product', 'action' => 'show']);
+$router->add('contact', ['controller' => 'Contact', 'action' => 'index']);
 $router->add('{controller}/{action}');
 
 /*
